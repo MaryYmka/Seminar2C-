772d22fb56bd2dab2e246c32f5c7f6029646c5ce
Maria <skorynina.m@yandex.ru>
2023-12-21 19:18:22 +0300

    Четвертая задача

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принемает число на вход и проверятет, является ли число кратным 7 и 23
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if((num1 % 7 ==0) & (num1 % 23 ==0))
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
