909017430d950687ca0e3d58cb1c0d2a6387075b
Maria <skorynina.m@yandex.ru>
2023-12-21 17:24:11 +0300

    Вторая задача

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,11 +1,11 @@
 ﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99]
 //и показывает наибольшую цифру числа
 
-int number = GetNumber();
-Console.WriteLine(number);
+int number = GetNumber(); // Выдаем ранндомное чило
+Console.WriteLine(number);// Выводим рандомное число
 
-int num1 = number / 10; //получаем 1 число
-int num2 = number % 10; // получаем 2 число
+int num1 = number / 10; //получаем 1 число например, 78 / 10 =7
+int num2 = number % 10; // получаем 2 число, 78 % 10 = 8
 
 if (num1 > num2)
 {
@@ -16,7 +16,7 @@ else
     System.Console.WriteLine(num2);
 }
 
-int GetNumber()
+int GetNumber() //Рандом
 {
     Random rand = new Random();
     int num = rand.Next(10, 100);
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая выдает 3х значное число и удалет 2е.
+
+int number = new Random(). Next(100, 1000); //Получаем трехзначное число
+Console.WriteLine(number);
+int num1 = number / 100;
+int num2 = number % 10;
+
+int result = num1 * 10 + num2;
+Console.WriteLine(result);
+
